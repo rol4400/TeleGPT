3d71698775f8eff59f491ed7b59a5042e6f901b8
Ryan Olsen <rol4400@outlook.com>
2023-09-14 05:37:18 +1000

    Basic searching of chatrooms works well

--- src/index.ts
+++ src/index.ts
@@ -19,15 +19,8 @@ const apiHash = process.env.TELE_API_HASH;
 const session = new StringSession(process.env.TELE_STR_SESSION);
 const client = new TelegramClient(session, apiId, apiHash, {});
 
-const searchTelegramMessages = async ({ query }:any) => {
-	const filter = new Api.InputMessagesFilterEmpty({});
-	return await searchTelegramGlobal(query, filter);
-}
-
-const searchTelegramChatroomMessages = async ({ query, chat_id }:any) => {
-	const filter = new Api.InputMessagesFilterEmpty({});
-	return await searchTelegramByChat(query, filter, chat_id);
-}
+// DEV Input
+const input = require("input"); // npm i input
 
 const searchForTelegramChatroom = async ({ query }:any) => {
 	var result = await client.invoke(
@@ -37,17 +30,22 @@ const searchForTelegramChatroom = async ({ query }:any) => {
         })
       );
 
-      const query_result = result.chats.map(chat => {
+      const query_result:object = result.chats.map(chat => {
         return {
 			"Title": chat.title,
 			"ChatID": -Number(chat.id.value)
 		}
       })
 
-	  return query_result
+	  return JSON.stringify(query_result[0])
 }
 
-const searchTelegramByChat = async (query:any, filter:any, chat_id:any) => {
+const searchTelegramByChat = async ({ query, chat_id }:any) => {
+	const filter = new Api.InputMessagesFilterEmpty({});
+
+	console.log("DEBUG ******************************************************************")
+	console.log(query);
+	console.log(chat_id);
 	
 	try {
 		const result = await client.invoke(
@@ -65,19 +63,21 @@ const searchTelegramByChat = async (query:any, filter:any, chat_id:any) => {
 			})
 		  );		
 
-		return JSON.stringify(formatChatSearchResults(result));
+		return JSON.stringify(await formatChatSearchResults(result));
 	  } catch (error) {
 		return JSON.stringify(error);
 	  }
 
 }
 
-const searchTelegramGlobal = async (query:any, filter:any) => {
+const searchTelegramGlobal = async ({ query }:any) => {
+	const filter = new Api.InputMessagesFilterEmpty({});
+
 	try {
 		const result = await client.invoke(
 			new Api.messages.SearchGlobal({
 			  q: query,
-			  filter: new Api.InputMessagesFilterEmpty({}),
+			  filter: filter,
 			  minDate: -1,
 			  maxDate: -1,
 			  offsetRate: 0,
@@ -88,61 +88,69 @@ const searchTelegramGlobal = async (query:any, filter:any) => {
 			})
 		  );
 	
-		
-
-		return JSON.stringify(formatChatSearchResults(result));
+		return JSON.stringify(await formatChatSearchResults(result));
 	  } catch (error) {
 		return JSON.stringify(error);
 	  }
 }
 
 const formatChatSearchResults = async (result: any) => {
+
+	console.log(result);
+
 	const query_result = result.messages.map(message => {
 		//result.users.find(user => user.id === message.fromId)?.username || 'N/A';
 		
-		var username:string = "";
-		var chat:string;
-		var messageText:string;
+		try {
+			var username:string = "";
+			var chat:string;
+			var messageText:string;
 
-		// Check if peerId has channelId, otherwise, set chat to 'N/A'
-		if (message.peerId.className == "PeerChannel") {
-		   chat = result.chats.find(chat => chat.id.value === message.peerId.channelId.value).title
+			// Check if peerId has channelId, otherwise, set chat to 'N/A'
+			if (message.peerId.className == "PeerChannel") {
+			chat = result.chats.find(chat => chat.id.value === message.peerId.channelId.value).title
 
-		   if (message.fromId.className == "PeerUser") {
-				const user = result.users.find(user => user.id.value === message.fromId.userId.value)
-				const firstName = user?.firstName || 'N/A';
-				const lastName = user?.lastName || '';
+			if (message.fromId.className == "PeerUser") {
+					const user = result.users.find(user => user.id.value === message.fromId.userId.value)
+					const firstName = user?.firstName || 'N/A';
+					const lastName = user?.lastName || '';
 
-				// Create the username by combining firstName and lastName
-				username = (firstName && lastName) ? `${firstName} ${lastName}` : firstName || lastName || 'N/A';
+					// Create the username by combining firstName and lastName
+					username = (firstName && lastName) ? `${firstName} ${lastName}` : firstName || lastName || 'N/A';
 
-		   }
-		} else {
-				chat = "N/A"
-				const user = result.users.find(user => user.id.value === message.peerId.userId.value)
-				const firstName = user?.firstName || 'N/A';
-				const lastName = user?.lastName || '';
+			}
+			} else {
+					chat = "N/A"
+					const user = result.users.find(user => user.id.value === message.peerId.userId.value)
+					const firstName = user?.firstName || 'N/A';
+					const lastName = user?.lastName || '';
 
-				// Create the username by combining firstName and lastName
-				username = (firstName && lastName) ? `${firstName} ${lastName}` : firstName || lastName || 'N/A';
+					// Create the username by combining firstName and lastName
+					username = (firstName && lastName) ? `${firstName} ${lastName}` : firstName || lastName || 'N/A';
 
-		}
+			}
 
-		// Define a maximum messageText length (e.g., 100 characters)
-		const maxMessageTextLength = 100;
-		messageText = message.message || '';
+			// Define a maximum messageText length (e.g., 100 characters)
+			const maxMessageTextLength = 100;
+			messageText = message.message || '';
 
-		// Truncate messageText if it exceeds the maximum length
-		if (messageText.length > maxMessageTextLength) {
-		messageText = messageText.slice(0, maxMessageTextLength) + '...';
-		}
-			  
-		return {
-		  username,
-		  chat,
-		  messageText,
-		};
+			// Truncate messageText if it exceeds the maximum length
+			if (messageText.length > maxMessageTextLength) {
+			messageText = messageText.slice(0, maxMessageTextLength) + '...';
+			}
+				
+			return {
+				username,
+				chat,
+				messageText,
+			};
+		} catch (error) {}
 	  });
+
+	  console.log("QUERY:")
+	  console.log(query_result);
+
+	return query_result.reverse();
 }
 
 (async function run() {
@@ -164,40 +172,26 @@ const formatChatSearchResults = async (result: any) => {
 			schema: z.object({
 				query: z.string().describe("The search query, keep it brief and simple"),
 			}),
-			func: searchTelegramMessages
+			func: searchTelegramGlobal
 			}),
 
 		new DynamicStructuredTool({
 			name: "telegram-message-search-by-chatroom",
-			description: "Searches through a specific telegram chatroom for the given query and returns the results in order of relevance. Use this instead of the telegram-message-search-global option if a chat ID is given. Chat ID MUST be given as a negative 8-10 digit number otherwise don't use this one",
+			description: "Searches through a specified telegram chatroom for the given query and returns the results in order of relevance. Use this instead of the telegram-message-search-global option ONLY if a 9 digit chat_id is known. chat_id MUST be given as a negative 9 digit number. Query can't be blank",
 			schema: z.object({
-				query: z.string().describe("The search query, keep it brief and simple"),
-				chat_id: z.number().describe("The ID of the chatroom. It should be a negative number with about 9 digits")
+				query: z.string().describe("The search query, keep it brief and simple. Can't be empty"),
+				chat_id: z.number().describe("The ID of the chatroom. It MUST be a negative number with 9 digits")
 			}),
-			func: searchTelegramChatroomMessages
+			func: searchTelegramByChat
 			}),
 
 		new DynamicStructuredTool({
 			name: "telegram-chatroom-search",
-			description: "Searches telegram chatrooms by query and will return the name of the chatrooms and their corresponding ID in order of relevance",
+			description: "Searches telegram chatrooms by query and will return the name of the chatrooms and their corresponding 9 digit chat ID",
 			schema: z.object({
 				query: z.string().describe("The search query, keep it brief and simple"),
 			}),
 			func: searchForTelegramChatroom
-			}),
-			
-
-		new DynamicStructuredTool({
-			name: "random-number-generator",
-			description: "generates a random number between two input numbers",
-			schema: z.object({
-				low: z.number().describe("The lower bound of the generated number"),
-				high: z.number().describe("The upper bound of the generated number"),
-			}),
-			func: async ({ low, high }) => {
-				return "hi";
-			}, // Outputs still must be strings
-			returnDirect: false, // This is an option that allows the tool to return the output directly
 			}),
 	];
 
@@ -214,9 +208,12 @@ const formatChatSearchResults = async (result: any) => {
 		},
 	});
 
-	const result = await executor.call({ input: `Find me the latest post in my 152 basket` });
-
-	console.log(result);
+	while (true) {
+		const prompt = await input.text("Please enter the prompt: ") 
+		const result = await executor.call({ input: prompt });
+	
+		console.log(result);
+	}
 
 	// const result2 = await executor.call({
 	// 	input: `what is that number squared?`,
