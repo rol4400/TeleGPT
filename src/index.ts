d9ac9452692577e38e7fedc49400ff819e645859
Ryan Olsen <rol4400@outlook.com>
2023-09-16 09:03:21 +1000

    Converted agent into an object to allow multiple agents

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,19 @@ require('dotenv').config();
 const { Telegraf } = require('telegraf');
 const marked = require ('marked');
 const axios = require("axios");
-const agent = require("./agent.js");
+const Agent = require("./agent");
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
+
+var agent = new Agent();
+agent.init();
 
 bot.on('voice', (ctx:any) => {
     ctx.sendChatAction('typing');
     ctx.telegram.getFileLink(ctx.message.voice.file_id).then((link:string) => {
         //link = https://api.telegram.org/file/bot<token>/<file_id>
-                
+          
+        // Download the voice file
         axios({
             url: link,
             method: "GET",
@@ -25,9 +29,13 @@ bot.on('voice', (ctx:any) => {
                 },
             };
             
+            // Use IBM to transcribe the speech to text
             axios.post(process.env.SPEECH_TO_TEXT_URL + "/v1/recognize", get_response.data, postHeaders)
             .then(async (post_response:any) => {
-                var answer = await agent.runQuery(post_response.data.results[0].alternatives[0].transcript);
+                // Edit the message and put the transcribed text as a caption
+
+                // Ask the agent and wait for the response
+                var answer = await agent.run(post_response.data.results[0].alternatives[0].transcript);
                   return ctx.replyWithHTML(marked.parseInline(answer.output));
               })
               .catch(function(error:any){
@@ -40,7 +48,7 @@ bot.on('voice', (ctx:any) => {
 
 bot.on('text', async (ctx:any) => {
     await ctx.persistentChatAction("typing", async () => {
-        var answer = await agent.runQuery(ctx.message.text);
+        var answer = await agent.run(ctx.message.text);
         return ctx.replyWithHTML(marked.parseInline(answer.output));
     })
 })
